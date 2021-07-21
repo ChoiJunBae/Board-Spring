c7a36f7fe8a29c19d924a319ee76b9b2bb8e9397
ChoiJunBae <cjb9610@naver.com>
2021-07-21 09:05:16 +0900

    유튜브 오류에 의해 코드 백업

--- board/front/src/views/list.vue
+++ board/front/src/views/list.vue
@@ -5,12 +5,10 @@
                 class="ma-5"
                 v-model="search"
                 label="검색할 노래를 입력하세요."
-                    v-on:keyup.enter="making()"
                 required
                 outlined
             ></v-text-field>
-                 
-            <v-btn @click="making()">dd</v-btn>
+            <v-btn @click="making()">제출</v-btn>
 		</v-row>
         <v-row>
             <v-col v-for="(player,index) in players" :key="index" cols="3" sm="6" md="4" lg="3" >
@@ -26,22 +24,17 @@
 import axios from 'axios'
 
 export default {
-    mounted(){
+    created(){
+        this.before();
         this.youtube();
-       
-       
-
-    },
-    beforeDestroy(){
-               this.setting()
     },
     data: function(){
         return{
             src:[],
             list:[], 
             players:[], // 상단 v-for에서 사용되는 배열
-            search:undefined,  // 검색 창에서 입력되어지는 검색어
-            source:'d',  // youtube메소드 논리 연산을 위한 객체
+            search:'',  // 검색 창에서 입력되어지는 검색어
+            source:'',  // youtube메소드 논리 연산을 위한 객체
             condition:'' // 날씨 정보
         }
     },
@@ -52,13 +45,29 @@ export default {
             this.$router.go(0);
         },
 
+        before(){
+            console.log('헤응');
+            const name = localStorage.getItem('name');
+            this.condition = localStorage.getItem('condition');
+            this.source = localStorage.getItem('search');
+            
+            if(this.condition!=null&&name!=null){
+                if(this.source==null){
+                    this.source = this.condition+' 노래';
+                }else{
+                    this.source = localStorage.getItem('search');
+                }
+            }else{
+                alert('로그인 후 이용이 가능합니다. 로그인 부탁드립니다.')
+                this.$router.push('/')
+            }
+        },
         // 페이지 시작 시 바로 시작되며 저장되었던 search있다면 그것을 검색하고 아니라면 날씨에 따라서 검색함
         async youtube(){
             
-
             // 검색어 인코딩후 url에 부착하여 axios get 요청 진행
             const encodingSearch = encodeURI(this.source);
-            const fullUrl="https://www.googleapis.com/youtube/v3/search?part=snippet&q="+encodingSearch+"&key=AIzaSyCi9gnSKw2wgpuh0X04YiS-3GlC6lSCoo8&maxResults=2"
+            const fullUrl="https://www.googleapis.com/youtube/v3/search?part=snippet&q="+encodingSearch+"&key=AIzaSyC-vIZ3-hbvv-gEsUEhgMvWTqtvyGPK0NE&maxResults=2"
             const video = await axios.get(fullUrl);
             console.log(fullUrl);
 
@@ -75,23 +84,6 @@ export default {
                 }
             }
         },
-
-        setting(){
-            const name = localStorage.getItem('name');
-            this.condition = localStorage.getItem('condition');
-            this.source = localStorage.getItem('search');
-            
-            if(this.condition!=null&&name!=null){
-                if(this.source==null){
-                    this.source = this.condition+' 노래';
-                }else{
-                    this.source = localStorage.getItem('search');
-                }
-            }else{
-                alert('로그인 후 이용이 가능합니다. 로그인 부탁드립니다.')
-                this.$router.push('/')
-            }
-        }
     }
 }
 </script>
